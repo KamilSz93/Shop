8edf9fdeb01dec13fc618eda65a6f2d4cefa0e5d
Kamil Sz <kamil.szewczyk9304@gmail.com>
2022-07-29 22:22:18 +0200

    Add ShopCard component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,51 @@
 import Card from 'react-bootstrap/Card';
 import Button from "react-bootstrap/Button";
 
-const quantity = 0
+const quantity = 0;
 
-const ShopCard = () => {
-    return (
-      <Card className="w-100">
-        <Card.Img
-          variant="top"
-          src="/imgs/banana.jpg"
-          style={{ objectFit: "cover", height: "150px" }}
-        />
-        <Card.Body className="d-flex flex-column">
-          <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-            <span className="fs-2">Banan</span>
-            <span className="ms-2 text-muted">20$</span>
-                </Card.Title>
-                <div className="mt-auto">
-                    {quantity === 0 ? 
-                    (<Button className="w-100">+ Add to card</Button>) : null}
-                </div>
-        </Card.Body>
-      </Card>
-    );
+type ItemProps = {
+  id: number
+  name: string
+  price: number
+  imgUrl: string  
 }
+
+const ShopCard = ({ id, name, price, imgUrl }:ItemProps) => {
+  return (
+    
+    <Card className="w-100">
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        style={{ objectFit: "cover", height: "150px" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{ name }</span>
+          <span className="ms-2 text-muted">{ price }</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity ? (
+            <Button className="w-100">+ Add to card</Button>
+          ) : (
+            <div className="d-flex flex-column  align-items-center">
+              <div className="d-flex align-items-center justify-content-center">
+                <Button>+</Button>
+                <div>
+                  <span className="ms-2 me-2">1 in Card</span>
+                </div>
+                <Button>-</Button>
+              </div>
+              <Button className="mt-2" size="sm" variant="danger">
+                {" "}
+                Remove{" "}
+              </Button>
+            </div>
+          )}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default ShopCard;
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,10 +1,15 @@
-import ShopCard from '../components/Card'
+import ShopCard from '../components/Card';
+import data from '../data/data.json';
+import { Row, Col } from 'react-bootstrap';
 
 const Shop = () => {
-    return (
-        <div>
-            <ShopCard />
-     </div>
- )   
+
+    return <Row xs={1} md={2} lg={3} className="g-3">
+        {data.map(itemData => (
+            <Col key={itemData.id}>
+                <ShopCard {...itemData} />
+            </Col>
+        ))}
+    </Row>;   
 }
 export default Shop;
